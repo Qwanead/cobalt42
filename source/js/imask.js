a2c87d92e9865c40256285a9ebdfa6a4528b0d43
Антон <anton@cobalt42.ru>
2022-06-29 09:17:58 +0500

    Добавляет imask

---
+++ source/js/imask.js
@@ -0,0 +1,17 @@
+import IMask from 'imask';
+
+const telephoneEl = document.querySelector('#tel-id');
+
+if (telephoneEl) {
+  IMask(telephoneEl, { mask: '+{7} (000) 000-00-00' });
+
+  telephoneEl.addEventListener('input', function () {
+    if (telephoneEl.validity.patternMismatch) {
+      telephoneEl.setCustomValidity(
+        'Введите номер телефона в формате: +7 (123) 456-78-90',
+      );
+    } else {
+      telephoneEl.setCustomValidity('');
+    }
+  });
+}
