d10113d78e38cd1a8e97c637966f5693aa0c6b0b
Антон <anton@cobalt42.ru>
2022-07-04 11:04:42 +0500

    Добавляет адаптив к contacts, закрытие по esc и клику вне окна

--- source/js/contacts.js
+++ source/js/contacts.js
@@ -2,18 +2,35 @@ const contactsLinkEl = document.querySelector('#contacts-link');
 const contactsEl = document.querySelector('#contacts');
 const closeBtnEl = document.querySelector('#contacts-close');
 
-const opneModal = evt => {
+const onEscKeyDown = evt => {
+  if (evt.key === 'Escape') {
+    closeModal();
+  }
+};
+
+const onOverlayClick = evt => {
+  if (evt.target.id === 'contacts') {
+    closeModal();
+  }
+};
+
+const openModal = evt => {
   evt.preventDefault();
   contactsEl.classList.add('contacts--visible');
   closeBtnEl.addEventListener('click', closeModal);
+  window.addEventListener('keydown', onEscKeyDown);
+  contactsEl.addEventListener('click', onOverlayClick);
+  document.body.style.overflow = 'hidden';
 };
 
-const closeModal = evt => {
-  evt.preventDefault();
+const closeModal = () => {
   contactsEl.classList.remove('contacts--visible');
   closeBtnEl.removeEventListener('click', closeModal);
+  window.removeEventListener('keydown', onEscKeyDown);
+  contactsEl.removeEventListener('click', onOverlayClick);
+  document.body.style.overflow = 'auto';
 };
 
-if (contactsEl && contactsEl) {
-  contactsLinkEl.addEventListener('click', opneModal);
+if (contactsEl && closeBtnEl && contactsLinkEl) {
+  contactsLinkEl.addEventListener('click', openModal);
 }
