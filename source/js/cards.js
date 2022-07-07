78042b9abaeeb74370366044ea60ec92d790a739
Антон <anton@cobalt42.ru>
2022-07-07 11:13:06 +0500

    Изменяет анимацию для перекючения карточек

--- source/js/cards.js
+++ source/js/cards.js
@@ -1,26 +1,16 @@
 const cardBtnEls = document.querySelectorAll('.cards__btn');
-const functionalBtnEl = document.querySelector('.functional__btn');
-const functionalCardEl = document.querySelector('.functional__api');
-
-const toggleCard = evt => {
-  const cardsItem = evt.target.closest('.cards__item');
-  const cardBtnEl = evt.target.closest('.cards__btn');
-
-  cardsItem.classList.toggle('cards__item--active');
-  const isCardActive = cardsItem.classList.contains('cards__item--active');
-  cardBtnEl.textContent = isCardActive ? 'Скрыть -' : 'Подробнее +';
-};
+const functionalBtnEls = document.querySelectorAll('.functional__btn');
+const functionalCardEl = document.querySelector('.functional__api-wrapper');
 
 [...cardBtnEls].forEach(cardBtnEl => {
-  cardBtnEl.addEventListener('click', toggleCard);
+  cardBtnEl.addEventListener('click', evt => {
+    const cardsItem = evt.target.closest('.cards__item');
+    cardsItem.classList.toggle('cards__item--active');
+  });
 });
 
-if (functionalBtnEl) {
+[...functionalBtnEls].forEach(functionalBtnEl => {
   functionalBtnEl.addEventListener('click', () => {
-    functionalCardEl.classList.toggle('functional__api--active');
-    const isCardActive = functionalCardEl.classList.contains(
-      'functional__api--active',
-    );
-    functionalBtnEl.textContent = isCardActive ? 'Скрыть -' : 'Подробнее +';
+    functionalCardEl.classList.toggle('functional__api-wrapper--active');
   });
-}
+});
